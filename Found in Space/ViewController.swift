7e782030ecdf7156c837c60087ac38ebe04f19f7
Theo Mendes <ghostcitizen@protonmail.ch>
2018-10-20 09:04:49 -0300

    Initial Project

---
+++ Found in Space/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Found in Space
+//
+//  Created by Theo Mendes on 20/10/18.
+//  Copyright © 2018 NASA Space Apps 2018. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'Found in Space' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Found in Space
+
+  target 'Found in SpaceUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
