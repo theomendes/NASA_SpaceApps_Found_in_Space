bb01c5a2c6bd5a549ffbd46f488d3e3b1068d430
Theo Mendes <ghostcitizen@protonmail.ch>
2018-10-20 18:19:20 -0300

    Added fonts and login controller

---
+++ Found in Space/Extensions/UIFont.swift
@@ -0,0 +1,14 @@
+//
+//  UIFont.swift
+//  Found in Space
+//
+//  Created by Theo Mendes on 20/10/18.
+//  Copyright © 2018 NASA Space Apps 2018. All rights reserved.
+//
+import UIKit
+
+extension UIFont {
+    class var homeBtn81: UIFont {
+        return UIFont(name: "Oxygen-Bold", size: 81.0)!
+    }
+}
